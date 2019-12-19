5dbdf90e6f6940627a56a029cde320ec60c407a2
Junior Miksza <junior.miksza8@gmail.com>
2019-12-19 16:45:09 -0300

    Pedido,pagamento e estado de pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -36,11 +36,14 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	List<Pedido> pedidos = new ArrayList<>();
+	
+
 	public Cliente() {
 		
 	}
-	
-	
 	
 	public Cliente(Integer id, String nome, String emai, String cpfOuCpnj, TipoCliente tipo) {
 		super();
@@ -134,6 +137,17 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 
 
 
---
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,32 @@
+package com.nelioalves.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento{
+	private static final long serialVersionUID = 1L;
+	private Integer NumeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido,Integer NumeroDeParcelas) {
+		super(id, estado, pedido);
+		this.NumeroDeParcelas = NumeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return NumeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		NumeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+	
+}
